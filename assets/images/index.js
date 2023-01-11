b430c316aa0a4f9781425553cd90d2469ffdc243
Chris-Imade <jephthahimade@gmail.com>
2023-01-11 07:19:53 +0100

    Created a new Expo app

---
+++ assets/images/index.js
@@ -0,0 +1,9 @@
+import Logo from  "./app-logo.png";
+import Blur from "./blur.png";
+import Persons from "./person.png";
+
+export const images = {
+    Logo,
+    Blur,
+    Persons,
+}
---
+++ components/shared.js
@@ -0,0 +1,10 @@
+import { Dimensions } from "react-native";
+
+export const colors = {
+    primary: "#FF4B3A",
+    white: "#ffffff"
+}
+
+export const ScreenHeight = Dimensions.get("screen").height;
+export const ScreenWidth = Dimensions.get("screen").width;
+export const WindowHeight = Dimensions.get("window").height;
